6d65545cf2fdfcce4aab4d6e288a50e263cae9b8
alejandroricardolund-cell <alejandroricardolund@gmail.com>
2026-05-11 14:41:50 +0100

    Create route.ts

---
+++ src/app/api/uploadthing/route.ts
@@ -0,0 +1,23 @@
+import { createRouteHandler } from "uploadthing/next";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  fileUploader: f({
+    maxFileSize: "32MB",
+    maxFileCount: 10,
+  })({
+    middleware: async () => {
+      return { userId: "admin" };
+    },
+    onUploadComplete: async ({ file }) => {
+      return { uploadedBy: "admin" };
+    },
+  }),
+};
+
+export type OurFileRouter = typeof ourFileRouter;
+
+export const { GET, POST } = createRouteHandler({
+  router: ourFileRouter,
+});
